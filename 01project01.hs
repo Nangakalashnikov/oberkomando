76bca898850f02ed551b0e23a78491edbc4eb654
Nanga PS <nangaparbat29@yahoo.co.id>
2015-09-12 02:20:32 +0700

    Update 01project01.hs
    @skadinyo @SabdaPS

--- 01project01.hs
+++ 01project01.hs
@@ -1,4 +1,248 @@
-modul ware 01
+module One where 
+
+import Data.List
+-- I'm so sorry before if i didn't add ' after the function, it cause i used Heat 
+-- 1.a
 
 null [] = True
-null _ = False
+null Num = False
+null [Num] = False
+null (Num) = False
+null Char = False
+null [Char] = False
+null (Char) = False
+null :: Foldable t => t a -> Bool
+
+--pembatas
+
+take:: Int -> [a] -> [a]
+take 1 [1]
+take 2 [2]
+take 2 [1,2] = [1,2]
+take 3 [1,2,3,4] = [1,2,3,4]
+
+--pembatas
+
+drop x = x
+--pembatas
+
+fst' x = x
+
+--pembatas
+
+snd' x = x
+
+--pembatas
+
+map' x = x
+
+--pembatas
+
+filter' x = x
+
+--pembatas
+
+delete' x = x
+
+--pembatas
+
+deleteAll' x = x
+
+--pembatas
+
+foldl' x = x
+
+--pembatas
+
+foldl1' x = x
+
+--pembatas
+
+zip' x = x
+
+--pembatas
+
+zipWith' x = x
+
+--pembatas
+
+nth' x = x
+
+--pembatas
+
+scanl' x = x
+
+--pembatas
+
+scanl1' x = x
+
+--pembatas
+
+elem' x = x
+
+--pembatas
+
+notElem' x = x
+
+--pembatas
+
+head' x = x
+
+--pembatas
+
+length' x = x
+
+--pembatas
+
+reverse' x = x
+
+--pembatas
+
+last' x = x
+
+--pembatas
+
+tail' x = x
+
+--pembatas
+
+init' x = x
+
+--pembatas
+
+max' x = x
+
+--pembatas
+
+min' x = x
+
+--pembatas
+
+concat' x = x
+
+--pembatas
+
+intersperse' x = x
+
+--pembatas
+
+intercalate' x = x
+
+--pembatas
+
+and' x = x
+
+--pembatas
+
+or' x = x
+
+--pembatas
+
+zip3' x = x
+
+--pembatas
+
+sum' x = x
+
+--pembatas
+
+product' x = x
+
+--pembatas
+
+words' x = x
+
+--pembatas
+
+lines' x = x
+
+--pembatas
+
+unlines' x = x
+
+--pembatas
+
+unwords' x = x
+
+--pembatas
+
+takeWhile' x = x
+
+--pembatas
+
+dropWhile' x = x
+
+--pembatas
+
+concatMap' x = x
+
+--pembatas
+
+all' x = x
+
+--pembatas
+
+any' x = x
+
+--pembatas
+
+insert' x = x
+
+--pembatas
+
+zipWith3' x = x
+
+--pembatas
+
+-- 1.b
+
+nub' x = x
+
+--pembatas
+
+sort' x = x
+
+--pembatas
+
+minimum' x = x
+
+--pembatas
+
+maximum' x = x
+
+--pembatas
+
+inits' x = x
+
+--pembatas
+
+tails' x = x
+
+--pembatas
+
+union' x = x
+
+--pembatas
+
+intersect' x = x
+
+--pembatas
+
+group' x = x
+
+--pembatas
+
+splitAt' x = x
+
+--pembatas
+
+partition' x = x
+
+--pembatas
+
+replicate' x = x
+
+--pembatas
+-- First Assignment
+-- Reimplement Haskell function
+-- DON'T USE GOOGLE
